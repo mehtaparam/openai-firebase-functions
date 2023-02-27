0d3830f82720d2b4be2a49ee0b99c781ddf595c4
codingmantra <param@codingmantra.com>
2023-02-27 16:15:11 +0530

    Added CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,11 @@
 import * as functions from "firebase-functions";
 import { Configuration, OpenAIApi } from "openai";
 
+import * as cors from 'cors';
+
+const corsHandler = cors({ origin: true });
+
+
 const DEFAULT_MODAL = "text-davinci-003";
 
 const configuration = new Configuration({
@@ -12,19 +17,22 @@ const openai = new OpenAIApi(configuration);
 
 // eslint-disable-next-line max-len
 export const openAIHttpFunction = functions.https.onRequest(async (request, response) => {
-    const params = request.query;
+    corsHandler(request, response, async () => {
+        const params = request.query;
 
-    if (!params.query) {
-        response.send("Please Send Query Paramater");
-    }
+        if (!params.query) {
+            response.send("Please Send Query Paramater");
+        }
 
-    const openAPIResponse = await openai.createCompletion({
-        model: process.env.MODAL || DEFAULT_MODAL,
-        prompt: params?.query?.toString() || "Not Defined",
-        max_tokens: +(process.env.MAX_TOKENS || 0),
-        temperature: +(process.env.TEMPRATURE || 0),
+        const openAPIResponse = await openai.createCompletion({
+            model: process.env.MODAL || DEFAULT_MODAL,
+            prompt: params?.query?.toString() || "Not Defined",
+            max_tokens: +(process.env.MAX_TOKENS || 0),
+            temperature: +(process.env.TEMPRATURE || 0),
+        });
+
+        response.send(JSON.stringify(openAPIResponse.data));
     });
 
-    response.send(JSON.stringify(openAPIResponse.data));
 });
 
--- readme.md
+++ readme.md
@@ -11,14 +11,14 @@ Use > Use this Template option from GitHub
 
 Steps:
 
-1. Register your self in OpenAI
-2. Create project in Google Firebase Console
+1. Register your self in OpenAI (<https://platform.openai.com/>)
+2. Create project in Google Firebase Console  (<https://console.firebase.google.com/>)
 3. Upgrade your Firebase project to Blaze Plan
 4. You must already have setup Node, Git, VSCode in your system
-5. Install Firebase tools
-6. Firebase Login from terminal
-7. Firebase Init in your project directory
+5. Install Firebase tools (npm install -g firebase-tools )
+6. Firebase Login from terminal (firebase login)
+7. Firebase Init in your project directory (firebase init)
 8. Select Firebase functions / hosting (optional)
-9. Add dependencies which you will required for ex. openai
+9. Add dependencies which you will required for ex. openai (npm i openai --save)
 10. Edit index ts file in functions and write your own
 11. Deploy functions
